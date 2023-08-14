289fcb370af4b8cddfcefe900f59150839f35158
nepalankit <nepalankit16@gmail.com>
2023-08-14 08:21:24 +0545

    me

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-import uploadRoutes from "./routes/uploadRoutes.js";
+// import uploadRoutes from "./routes/uploadRoutes.js";
+
 import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
